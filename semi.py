35337b592c56f79f43dcbbf4709d6763c8721d30
TallYoda <joteshagi@gmail.com>
2020-05-03 18:27:13 -0700

    revamp 2.0

--- semi.py
+++ semi.py
@@ -1,124 +1,178 @@
-import json
 import logging
+import json
 
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import (Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters,
-                          ConversationHandler)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters, ConversationHandler)
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-ENTRY, ENTRY1, LOCATION, BYE= range(4)
-#callback data
-ONE = range(1)
-
+FIRST, SECOND, PHARMDESCRIPTION, PHARMLOCATION, ENTRY1, ENTRY2, LOCATION = range(7)
 class userdata(object):
     def __init__(self):
-        self.dict = {}
-        self.dict1 = {
-            'drug_name' : None,
-            'dosage' : None,
-            'longitude' : None,
-            'latitude' : None
+        self.dict = {
+            "chat_id" : None,
+            "drug_name" : None,
+            "dosage" : None,
+            "location" : {
+                "longitude" : None,
+                "latitude" : None
+                }
         }
-        self.dict2 = {'chatId' : None}
-userdata = userdata()
-
-def start(update, context):
-    chatId = update.message.chat.id
-    update.message.reply_text('please enter the name of the drug')
-    userdata.dict[chatId] = userdata.dict1
-    userdata.dict2['chatId'] = update.message.chat.id
-    return ENTRY
-
-def entry(update, context):
-    user = update.message.from_user
-    chatId = update.message.chat.id
-    userdata.dict[chatId]['drug_name'] = update.message.text
-
-    logger.info('%s entered %s for drugs ', user.first_name, update.message.text)
-    update.message.reply_text('Now Enter the dosage:---')
-    return ENTRY1
 
 
+class pharmdata(object):
+    def __init__(self):
+        self.dict = {
+            "chat_id" : None,
+            "description" : None,
+            "location" : {
+                    "longitude" : None,
+                    "latitude" : None,
+            }
+        }
+def write_json(data, filename='drugusers.json'):
+    with open(filename,'w') as f:
+        json.dump(data, f, indent=4)
+class methods():
+    def __init__(self):
+        pass
+    def start(update, context):
 
-def entry1(update, context):
-    user = update.message.from_user
-    chatId = update.message.chat.id
-    userdata.dict[chatId]['dosage'] = update.message.text
-    with open('drugusers.json', 'a') as f:
-        json.dump(userdata.dict, f, indent =2)
-    logger.info('%s entered %s for dosage ', user.first_name, update.message.text)
-    update.message.reply_text('please send your location')
-    return LOCATION
-
-def location(update, context):
-        bot = context.bot
-        chatId = update.message.chat.id
         keyboard = [
-            [InlineKeyboardButton("Available", callback_data= str(ONE))]
+            [InlineKeyboardButton("Pharmacist", callback_data= 'baby'),
+             InlineKeyboardButton("Customer", callback_data= 'gaga')]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text('gagagugu', reply_markup = reply_markup)
+        return FIRST
+    def acces(self, thing):
+        self.x = thing
+        return self.x
+    def entry(update, context):
+        logger.info('user has chosen customer ')
+        query = update.callback_query
+        #user = update.message.from_user
+        chatId = query.message.chat.id
+        query.edit_message_text(text="please Enter the name of the drug you wish to request")
+        #query.message.reply_text('please Enter the name of the drug you wish to request')
+        #update.reply_text('make sure you spell your query correctly, use google first if you have to')
+        userdta = userdata()
+        userdta.dict["chat_id"] = chatId
+        methods.acces(userdta)
+        return ENTRY1
+    def entry1(update, context):
         user = update.message.from_user
+
+        methods.x.dict["drug_name"] = update.message.text
+
+        logger.info('%s entered %s for drug_name ', user.first_name, update.message.text)
+        update.message.reply_text('please send the DOSAGE for the prescribed medicine')
+
+        return ENTRY2
+    def entry2(update, context):
+        user = update.message.from_user
+        methods.x.dict["dosage"] = update.message.text
+
+        logger.info('%s entered %s for dosage ', user.first_name, update.message.text)
+        update.message.reply_text('Enter the gps location')
+        return LOCATION
+
+
+    def typingdescription(update, context):
+        update.message.reply_text("Thank you very much, the info you input will help customers find your pharmacy easily"
+        "\nNow please send your location from within telegram by using the attachment option")
+        methods.x.dict["description"] = update.message.text
+
+        logger.info('user has sent location description')
+
+        return PHARMLOCATION
+    def pharmlocation(update,context):
         user_location = update.message.location
-        logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
-                    user_location.longitude)
-        userdata.dict[chatId]['latitude'] = user_location.latitude
-        userdata.dict[chatId]['longitude'] = user_location.longitude
-        update.message.reply_text('Thank you for using our bot '
-                                  'we will get back to you as soon as we can', reply_markup = ReplyKeyboardRemove())
-        with open('drugusers.json', 'a') as f:
-            json.dump(userdata.dict, f, indent =2)
+        update.message.reply_text('Thankyou for inputting your gps location.'
+        '\nFrom now on you will get updates of requested drugs through this bot')
+        methods.x.dict["location"]["longitude"] = user_location.longitude
+        methods.x.dict["location"]["latitude"] = user_location.latitude
+        update.message.reply_text('you can use /stop command to stop getting updates')
+        logger.info('user has sent gps location')
+        with open('pharmacist.json') as json_file:
+            data = json.load(json_file)
+            temp = data["users"]
+            temp.append(methods.x.dict)
+        write_json(data, filename = 'pharmacist.json')
+        return ConversationHandler.END
 
-        bot.send_message(chat_id = '@trial13', text = 'user : ' +user.first_name +'\nDrug Requested : ' + userdata.dict[chatId]['drug_name'] +'\nDosage : '
-                    + userdata.dict[chatId]['dosage'],
-                reply_markup = reply_markup)
+    def location(update, context):
+            bot = context.bot
+            keyboard = [
+                [InlineKeyboardButton("Available", callback_data = 'avail')]
+            ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            user = update.message.from_user
+            user_location = update.message.location
+            logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
+                        user_location.longitude)
+            methods.x.dict["location"]["latitude"] = user_location.latitude
+            methods.x.dict["location"]["longitude"] = user_location.longitude
+            update.message.reply_text('Thank you for using our bot '
+                                      'we will get back to you as soon as we can', reply_markup = ReplyKeyboardRemove())
+
+            with open('drugusers.json') as json_file:
+                data = json.load(json_file)
+                temp = data["users"]
+                temp.append(methods.x.dict)
+            write_json(data)
 
 
-        return ONE
-
-def available(update, context):
-
-    bot = context.bot
-    bot.send_message(chat_id = userdata.dict2.chatId,
-    text = 'your requested drug has been located at:__')
-    return ConversationHandler.END
+            f = open('pharmacist.json', "r")
+            pdata = json.load(f)
+            for i in pdata["users"]:
+                bot.send_message(chat_id = i["chat_id"], text = "requested drug is" + methods.x.dict["drug_name"] , reply_markup = reply_markup )
 
 
-def cancel(update, context):
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text('Bye! I hope we can talk again some day.',
-                              reply_markup=ReplyKeyboardRemove())
-    return ConversationHandler.END
+
+            return ConversationHandler.END
+    def pharmacist(update, context):
+        #chatId  = update.message.chat.id
+        logger.info('user just chose pharmacist')
+        query = update.callback_query
+        query.message.reply_text('If your a pharmacist and wish to get updates from customers please enter a description of your location')
+        logger.info('user just chose pharmacist')
+        pdata = pharmdata()
+        pdata.dict["chat_id"] = query.message.chat.id
+        methods.acces(pdata)
+
+        return PHARMDESCRIPTION
+
+
+    def cancel(update, context):
+        user = update.message.from_user
+        update.message.reply_text('you cancelled so early',reply_markup = ReplyKeyboardRemove())
+        return ConversationHandler.END
+
 def error(update, context):
-    """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-def main():
-        updater = Updater("1135978630:AAFTaXp1A33RrCxkfOpBsixZ7LxPp8FxYb4", use_context=True)
-        dp = updater.dispatcher
+updater = Updater("1135978630:AAFTaXp1A33RrCxkfOpBsixZ7LxPp8FxYb4", use_context=True)
+dp = updater.dispatcher
+conv_handler = ConversationHandler(
+    entry_points = [CommandHandler('start', methods.start)],
 
-        conv_handler = ConversationHandler(
-            entry_points = [CommandHandler('start', start)],
-            states = {
-                ENTRY : [MessageHandler(Filters.all, entry)],
-                ENTRY1 : [MessageHandler(Filters.all, entry1)],
-                LOCATION : [MessageHandler(Filters.location, location)],
-                ONE : [CallbackQueryHandler(available, pattern='^' + str(ONE) + '$')]
-            },
-
-            fallbacks = [CommandHandler('cancel',cancel)]
-        )
-
-
-        dp.add_handler(conv_handler)
-        dp.add_error_handler(error)
-
-        updater.start_polling()
-        updater.idle()
-if __name__== '__main__':
-    main()
+    states = {
+        FIRST : [CallbackQueryHandler(methods.pharmacist, pattern = '^baby', ),
+            CallbackQueryHandler(methods.entry, pattern = '^gaga', )],
+        ENTRY1 : [MessageHandler(Filters.all, methods.entry1)],
+        ENTRY2 : [MessageHandler(Filters.all, methods.entry2)],
+        LOCATION : [MessageHandler(Filters.location, methods.location)],
+        PHARMDESCRIPTION : [MessageHandler(Filters.all, methods.typingdescription)],
+        PHARMLOCATION : [MessageHandler(Filters.location, methods.pharmlocation)]},
+    fallbacks = [CommandHandler('cancel', methods.cancel)])
+methods = methods()
+dp.add_handler(conv_handler)
+dp.add_error_handler(error)
+updater.start_polling()
+updater.idle()
